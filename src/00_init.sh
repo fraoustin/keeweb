33674afe9a7939b6ae957dc35f505a9c006fb962
fraoustin <fraoustin@gmail.com>
2018-08-06 20:09:59 +0200

    correction manage param default

--- src/00_init.sh
+++ src/00_init.sh
@@ -4,6 +4,7 @@ cp /usr/share/nginx/html/index.html.ini /usr/share/nginx/html/index.html
 
 sed -i 's/"locale","en"/"locale","'$KEEWEB_LANG'"/g' /usr/share/nginx/html/index.html
 sed -i 's/activeLocale:"en"/activeLocale:"'$KEEWEB_LANG'"/g' /usr/share/nginx/html/index.html
+sed -i 's/,locale:null/,locale:'$KEEWEB_LANG'/g' /usr/share/nginx/html/index.html
 sed -i 's/||"en"/||"'$KEEWEB_LANG'"/g' /usr/share/nginx/html/index.html
 sed -i 's/"theme","fb"/"theme","'$KEEWEB_THEME'"/g' /usr/share/nginx/html/index.html
 sed -i 's/theme:"fb"/theme:"'$KEEWEB_THEME'"/g' /usr/share/nginx/html/index.html
@@ -18,5 +19,5 @@ sed -i 's/canOpen:!0/canOpen:!'$KEEWEB_OPEN'/g' /usr/share/nginx/html/index.html
 sed -i 's/canImportXml:!0/canImportXml:!'$KEEWEB_IMPORTXML'/g' /usr/share/nginx/html/index.html
 sed -i 's/canOpenSettings:!0/canOpenSettings:!'$KEEWEB_SETTINGS'/g' /usr/share/nginx/html/index.html
 sed -i 's/get("webdavSaveMethod")||"default"/get("webdavSaveMethod")||"'$KEEWEB_WEBDAVMETHOD'"/g' /usr/share/nginx/html/index.html
-sed -i 's|title:"openUrl"|title:"openUrl",value:"'$KEEWEB_WEBDAVURL'"|g' /usr/share/nginx/html/index.html
-sed -i 's/title:"openUser"/title:"openUser",value:"'$KEEWEB_WEBDAVUSER'"/g' /usr/share/nginx/html/index.html
+sed -i 's|title:"openUrl"|title:"openUrl",default:"'$KEEWEB_WEBDAVURL'"|g' /usr/share/nginx/html/index.html
+sed -i 's/title:"openUser"/title:"openUser",default:"'$KEEWEB_WEBDAVUSER'"/g' /usr/share/nginx/html/index.html
