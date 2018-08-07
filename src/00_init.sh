21ebb0c982beac72202f0c756e5345c927edea88
fraoustin <fraoustin@gmail.com>
2018-08-07 11:18:56 +0200

    add css

--- src/00_init.sh
+++ src/00_init.sh
@@ -4,7 +4,6 @@ cp /usr/share/nginx/html/index.html.ini /usr/share/nginx/html/index.html
 
 sed -i 's/KEEWEB_LANG/'$KEEWEB_LANG'/g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_THEME/'$KEEWEB_THEME'/g' /usr/share/nginx/html/index.html
-sed -i 's/KEEWEB_WEBDAV/'$KEEWEB_WEBDAV'/g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_DROPBOX/'$KEEWEB_DROPBOX'/g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_GDRIVE/'$KEEWEB_GDRIVE'/g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_ONEDRIVE/'$KEEWEB_ONEDRIVE'/g' /usr/share/nginx/html/index.html
@@ -17,3 +16,4 @@ sed -i 's/KEEWEB_SETTINGS/'$KEEWEB_SETTINGS'/g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_WEBDAVMETHOD/'$KEEWEB_WEBDAVMETHOD'/g' /usr/share/nginx/html/index.html
 sed -i 's|KEEWEB_WEBDAVURL|'$KEEWEB_WEBDAVURL'|g' /usr/share/nginx/html/index.html
 sed -i 's/KEEWEB_WEBDAVUSER/'$KEEWEB_WEBDAVUSER'/g' /usr/share/nginx/html/index.html
+sed -i 's/KEEWEB_WEBDAV/'$KEEWEB_WEBDAV'/g' /usr/share/nginx/html/index.html
